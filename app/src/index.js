c53ad4f5c1799627e6815783f7858ad51443af60
Govind Pagare <govinda155@gmail.com>
2026-08-12 18:15:15 +0530

    Update sample app message

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,6 @@ const port = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from test of cicd sample-app! Version: ' + (process.env.APP_VERSION || 'dev') + '\n');
+  res.end('Hello from Govind CI/CD sample-app! Version: ' + (process.env.APP_VERSION || 'dev') + '\n');
 }).listen(port, () => console.log(`Listening on ${port}`));
 // test change
