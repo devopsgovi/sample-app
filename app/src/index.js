36a06b9fcbdb3dc74131c7decafa89a7ec79c39c
Govind Pagare <govinda155@gmail.com>
2026-08-13 17:42:18 +0530

    Test automated CI CD deployment

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,6 @@ const port = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Govind CI/CD sample-app! Version: ' + (process.env.APP_VERSION || 'dev') + '\n');
+  res.end('Hello from Fully Automated CI/CD Pipeline!' + '\n');
 }).listen(port, () => console.log(`Listening on ${port}`));
 // test change
