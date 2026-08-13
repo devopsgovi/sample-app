92a8a6283eaa3259f35c0e3672d5c14ad44696a8
Govind Pagare <govinda155@gmail.com>
2026-08-13 18:27:46 +0530

    Add health endpoint and Kubernetes probes

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,18 @@
 const http = require('http');
+
 const port = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
+
+  if (req.url === '/health') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('OK\n');
+    return;
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Fully Automated CI/CD Pipeline!' + '\n');
-}).listen(port, () => console.log(`Listening on ${port}`));
-// test change
+  res.end('Hello from Fully Automated CI/CD Pipeline!\n');
+
+}).listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
